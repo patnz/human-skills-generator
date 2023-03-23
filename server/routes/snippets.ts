5141e96a1c6493de07083baa14faf8ebd00301e6
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-03-24 11:00:09 +1300

    finished all 3 internal routes

--- server/routes/snippets.ts
+++ server/routes/snippets.ts
@@ -17,4 +17,26 @@ router.get('/appSnippet', (req, res) => {
     })
 })
 
+router.get('/requirementSnippet', (req, res) => {
+  fs.readFile('./client/common/requirementSnippets.json')
+    .then((requirementSnippets) => {
+      const rand = _.random(0, requirementSnippets.length - 1)
+      res.json(requirementSnippets[rand])
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+router.get('/whatsOnTopSnippet', (req, res) => {
+  fs.readFile('./client/common/whatsOnTopSnippets.json')
+    .then((whatsOnTopSnippets) => {
+      const rand = _.random(0, whatsOnTopSnippets.length - 1)
+      res.json(whatsOnTopSnippets[rand])
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
 export default router
