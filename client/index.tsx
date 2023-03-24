909e0f66bcd7281f24155d307b2c76bd8b8b5178
Pat Wilson <pat@wilsons.org.nz>
2023-03-24 14:03:31 +1300

    clap component added

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
-// import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App'
-import Clap from './components/Clap'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(<Clap />)
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <Router>
+      <App />
+    </Router>
+  )
 })
