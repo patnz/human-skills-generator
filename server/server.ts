1afbf6f564d1e0990a458d6674779aacf13d27e0
Pat Wilson <pat@wilsons.org.nz>
2023-06-25 22:10:59 +1000

    updated config, for CORS

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,11 @@ import express from 'express'
 import cors, { CorsOptions } from 'cors'
 import snippets from './routes/snippets'
 
-const server = express()
+const server = express().use(
+  cors({
+    origin: '*',
+  })
+)
 
 server.use(express.json())
 server.use(express.static(join(__dirname, './public')))
